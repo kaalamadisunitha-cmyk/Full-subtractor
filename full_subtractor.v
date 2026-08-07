8ffcf46457f249ffdc0a736c16ab4a888fe12af2
kaalamadisunitha-cmyk <kaalamadisunitha@gmail.com>
2026-08-07 09:47:52 +0530

    Create full_subtractor.v

---
+++ full_subtractor.v
@@ -0,0 +1,12 @@
+module full_subtractor(
+    input A,
+    input B,
+    input Bin,
+    output D,
+    output Bout
+);
+
+assign D = A ^ B ^ Bin;
+assign Bout = (~A & B) | (~A & Bin) | (B & Bin);
+
+endmodule
